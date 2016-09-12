8b89ca17ff19cbcad1a5445742b3b7b7b5a6b339
root <root@localhost>
2016-09-12 12:17:03 +0200

    Temporarily remove SDL dependency

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
+//#define SDL
+
+#include <cstdio>
+
+#ifdef SDL
 #include <SDL2/SDL.h>
+#endif
+
 #include "namegen.h"
 #include "game.h"
+
+#ifdef SDL
 #include "sdl.h"
+#endif
 
 using namespace std;
 using namespace bb;
 
 int main() {
+#ifdef SDL
   if (!Init())
     return 1;
+#endif
+
   dwarves.insert(Dwarf::MakeRandom(0, 2));
-  dwarves.insert(Dwarf::MakeRandom(2, 5));
+//  dwarves.insert(Dwarf::MakeRandom(2, 5));
   AddStructure(1, 5, Structure::New(STAIRCASE));
   AddStructure(2, 5, Structure::New(STAIRCASE));
   AddStructure(3, 5, Structure::New(STAIRCASE));
@@ -19,10 +32,17 @@ int main() {
   AddStructure(3, 2, Structure::New(MUSHROOM_FARM));
   AddItem(Point(100, 800), SPORE);
 
+#ifdef SDL
   while (HandleInput()) {
     Tick();
     Draw();
   }
   SDL_Quit();
+#else
+  for (int i = 0; i < 30; ++i) {
+    printf("Tick %d\n", i);
+    Tick();
+  }
+#endif
   return 0;
 }
